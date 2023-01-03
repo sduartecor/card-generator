151f2d153ba0da846251f2339c8e96c5ac42873f
sduartecor <santiagoduartecor@outlook.com>
2023-01-03 18:45:54 +0000

    Final Version Proyecto

--- src/app.js
+++ src/app.js
@@ -13,12 +13,12 @@ window.onload = function() {
   generateRandomCard();
   //write your code here
   setInterval(() => {
-    //Obtener Numero Aleatorio
     generateRandomCard();
   }, 10000);
 };
 
 function generateRandomCard() {
+  //Obtener Numero Aleatorio
   let numbers = [
     "A",
     "2",
@@ -37,42 +37,26 @@ function generateRandomCard() {
 
   let indexNum = Math.floor(Math.random() * numbers.length);
 
+  //Insert Numero
   document.getElementById("number").innerHTML = numbers[indexNum];
 
-  //Obtener Simbolo Aleatorio
-  let option = Math.floor(Math.random() * 4) + 1;
-  let symbol_number;
-  switch (option) {
-    case 1: // Corazon
-      symbol_number = "♥";
-      document.getElementById("upper").style.color = "red";
-      document.getElementById("bottom").style.color = "red";
-      document.getElementById("number").style.color = "red";
-      break;
-    case 2: // Hoja
-      symbol_number = "♠";
-      document.getElementById("upper").style.color = "black";
-      document.getElementById("bottom").style.color = "black";
-      document.getElementById("number").style.color = "black";
-      break;
-    case 3: // Trebol
-      symbol_number = "♣";
-      document.getElementById("upper").style.color = "black";
-      document.getElementById("bottom").style.color = "black";
-      document.getElementById("number").style.color = "black";
-      break;
-    case 4: // Diamante
-      symbol_number = "♦";
-      document.getElementById("upper").style.color = "red";
-      document.getElementById("bottom").style.color = "red";
-      document.getElementById("number").style.color = "red";
-      break;
-    default:
-      break;
-  }
+  //Obenter Palo Aleatorio
+  let palos = ["♥", "♠", "♣", "♦"];
 
-  document.getElementById("upper").innerHTML = symbol_number;
-  document.getElementById("bottom").innerHTML = symbol_number;
+  let indexPal = Math.floor(Math.random() * palos.length);
+
+  document.getElementById("upper").innerHTML = palos[indexPal];
+  document.getElementById("bottom").innerHTML = palos[indexPal];
+
+  if (indexPal === 0 || indexPal === 3) {
+    document.getElementById("upper").style.color = "red";
+    document.getElementById("bottom").style.color = "red";
+    document.getElementById("number").style.color = "red";
+  } else {
+    document.getElementById("upper").style.color = "black";
+    document.getElementById("bottom").style.color = "black";
+    document.getElementById("number").style.color = "black";
+  }
 }
 
 document.getElementById("btnChange").addEventListener("click", () => {
